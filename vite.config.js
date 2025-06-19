0635536a787677d416371f1083105cc757947766
Andrea Bovo <spleen666@gmail.com>
2025-06-19 19:31:04 +0200

    add ssl support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import vitePluginString from 'vite-plugin-string';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+  server: {
+    https: true,
+  },
   plugins: [
+    basicSsl(),
     vitePluginString({
       include: '**/*.wgsl',
     }),
